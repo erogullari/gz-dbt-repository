1b420534407fc879b64eae183897a7683a01f195
erogullari <melikerogullari@gmail.com>
2026-06-29 00:09:19 +0300

    Add SQL query for sales margin calculation

--- models/intermediate/int_sales_margin.sql
+++ models/intermediate/int_sales_margin.sql
@@ -1,1 +1,14 @@
 
+select
+    sales.date_date,
+    sales.orders_id,
+    sales.products_id,
+    sales.revenue,
+    sales.quantity,
+    product.purchase_price,
+    round(sales.quantity * product.purchase_price, 2) as purchase_cost,
+    round(sales.revenue - sales.quantity * product.purchase_price, 2) as margin
+
+from {{ ref('stg_raw_sales') }} as sales
+left join {{ ref('stg_raw_product') }} as product
+    on sales.products_id = product.products_id
